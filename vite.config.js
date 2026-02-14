210de7fdc96481b61090bc095dd473df6fc66ac7
Thang Nguyen <thangnv1029@gmail.com>
2026-02-14 09:09:17 +0700

    Fix vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   // Cho phép vite sử dụng process.env thay vì import.meta.env
